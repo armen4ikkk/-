4ef47393cb1149491e4dca7f607126e8821d475a
armen <babayan_armenzzz@mail.ru>
2022-08-17 18:29:01 +0300

    задача№23

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите число: ");
+double N = double.Parse(ReadLine());
+
+Write($"{N} -> ");
+for(double i = 1; i <= N; i++) {
+    Write($"{Math.Pow(i, 3)}{(i != N ? "," : "\n")}");
+}
